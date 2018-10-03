2226f2e4ed0bb963f3a6c9cf89c35321c00babf4
brann0n <43607839+brann0n@users.noreply.github.com>
2018-10-03 13:38:58 +0200

    Added some basic html to work with.
    added form and input fields.

--- index.php
+++ index.php
@@ -6,12 +6,13 @@ Simon Gharnagharyan
 Date: 3-10-2018
 -->
 <?php
-        function DateProcessor(){
-            
+        function DateProcessor($stringWithDate){
+            return "";
         }
-        
-        function CallAPI(){
+        //Call this with your horoscope sign and the type (day ,week, year), it returns your current horoscope
+        function CallAPI($horoscopeSign, $hType){
             
+            return "";
         }
 ?>
 <html>
@@ -24,5 +25,14 @@ Date: 3-10-2018
         <?php
         
         ?>
+        <h1>PHP Horoscoop project</h1>
+        <div>Welcome to our horoscope website, please enter your date of birth and select the type of horoscope you would like to receive.</div>
+        <div>
+            <form action="welcome.php" method="post">
+                Name: <input type="text" name="name"><br>
+                E-mail: <input type="text" name="email"><br>
+                <input type="submit">
+            </form>
+        </div>
     </body>
 </html>
