aee884172ecd922265753248e795eebfb34f18d7
brann0n <43607839+brann0n@users.noreply.github.com>
2018-10-03 13:12:20 +0200

    Added index.php
    Index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Brandon Abbenhuis
+brandon.abbenhuis@student.nhlstenden.com
+Date: 
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
